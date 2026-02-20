e3a23e96278f6fe4be7ebc9a5afb13153f4d5b48
Nindyaull <pindangoreng123@gmail.com>
2026-02-20 17:15:48 +0700

    latihan2

---
+++ jobsheet2/Dosen22.java
@@ -0,0 +1,33 @@
+package jobsheet2;
+
+public class Dosen22 {
+    String idDosen;
+    String nama;
+    boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
+
+    void tampilInformasi() {
+        System.out.println("ID Dosen: " + idDosen);
+        System.out.println("Nama Dosen: " + nama);
+        System.out.println("Tahun Bergabung: " + tahunBergabung);
+        System.out.println("Bidang Keahlian: " + bidangKeahlian);
+    }
+
+    void setStatusAktif(boolean status) {
+        statusAktif = status;
+        if (status == true) {
+            System.out.println("Status Dosen: Aktif");
+        } else {
+            System.out.println("TStatus Dosen: idak Aktif");
+        }
+    }
+
+    int hitungMasaKerja(int thnSkrg) {
+        return thnSkrg - tahunBergabung;
+    }
+
+    void ubahKeahlian(String bidang) {
+        bidangKeahlian = bidang;
+    }
+}
