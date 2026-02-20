fc9746d3220e9579958f33c296cce11647ab53a5
Nindyaull <pindangoreng123@gmail.com>
2026-02-20 14:52:36 +0700

    object pt4

--- jobsheet2/MahasiswaMain22.java
+++ jobsheet2/MahasiswaMain22.java
@@ -48,6 +48,10 @@ public class MahasiswaMain22 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa22 mhs2 = new Mahasiswa22("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
     }
         
 }
