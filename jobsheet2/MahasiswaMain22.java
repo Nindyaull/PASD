eab8a25425534b0ca0501399cd327dfe9fdd438f
Nindyaull <pindangoreng123@gmail.com>
2026-02-20 15:41:16 +0700

    object pt5

--- jobsheet2/MahasiswaMain22.java
+++ jobsheet2/MahasiswaMain22.java
@@ -1,42 +1,6 @@
 package jobsheet2;
 
 public class MahasiswaMain22 {
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
-
-    void tampilkanInformasi() {
-        System.out.println("Nama: " + nama);
-        System.out.println("NIM: " + nim);
-        System.out.println("IPK: " + ipk);
-        System.out.println("Kelas: " + kelas);
-    }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIPK(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0,0 dan 4,0");
-        }
-    }
-
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
-
     public static void main(String[] args) {
         Mahasiswa22 mhs1 = new Mahasiswa22();
         mhs1.nama = "Muhammad Ali Farhan";
@@ -52,6 +16,9 @@ public class MahasiswaMain22 {
         Mahasiswa22 mhs2 = new Mahasiswa22("Annisa Nabila", "2141720160", 3.25, "TI 2L");
         mhs2.updateIPK(3.30);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa22 mhsNindya = new Mahasiswa22("Nindya", "254107060033", 3.90, "SIB 1D");
+        mhsNindya.tampilkanInformasi();
     }
         
 }
