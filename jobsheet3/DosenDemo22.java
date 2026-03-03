041fea97877554c35cf2c49b19e8b952ca208dad
Nindyaull <pindangoreng123@gmail.com>
2026-03-03 22:00:49 +0700

    dosen main no 1

---
+++ jobsheet3/DosenDemo22.java
@@ -0,0 +1,38 @@
+package jobsheet3;
+
+import java.util.Scanner;
+
+public class DosenDemo22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String kode, nama, jk, dummy;
+        boolean jenisKelamin;
+        int usia;
+        System.out.print("Masukkan jumlah data: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
+
+        Dosen22[] arrayOfDosen = new Dosen22[jumlah];
+
+        for (int i = 0; i < arrayOfDosen.length; i++) {
+            System.out.println("Masukkan Data Dosen ke-" + (i+1));
+            System.out.print("Kode          : ");
+            kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            nama = sc.nextLine();
+            System.out.print("Gender (L/P)  : ");
+            jk = sc.nextLine();
+            jenisKelamin = jk.equalsIgnoreCase("L");
+            System.out.print("Usia          : ");
+            dummy = sc.nextLine();
+            usia = Integer.parseInt(dummy);
+            System.out.println("---------------------------------------");
+
+            arrayOfDosen[i] = new Dosen22(kode, nama, jenisKelamin, usia);
+        }
+
+        for (Dosen22 dsn : arrayOfDosen) {
+            dsn.cetakInfo();
+        }
+    }
+}
