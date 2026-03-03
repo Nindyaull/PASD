e305d9530cdc4493a233b3082abb003e14b2e970
Nindyaull <pindangoreng123@gmail.com>
2026-03-03 16:00:27 +0700

    matkul class

---
+++ jobsheet3/Matakuliah22.java
@@ -0,0 +1,33 @@
+package jobsheet3;
+
+public class Matakuliah22 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah22() {
+    }
+
+    public Matakuliah22(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("SKS           : " + sks);
+        System.out.println("Jumlah Jam    : " + jumlahJam);
+        System.out.println("-----------------------------------");
+    }
+}
