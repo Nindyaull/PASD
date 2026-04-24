c03f294470daf6a3cdfb8aa6b27990cdacce79af
Nindyaull <pindangoreng123@gmail.com>
2026-04-24 18:43:32 +0700

    tugas mahasiswa stack

---
+++ jobsheet9/StackTugasMahasiswa22.java
@@ -0,0 +1,84 @@
+package jobsheet9;
+
+public class StackTugasMahasiswa22 {
+    Mahasiswa22[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa22(int size) {
+        this.size = size;
+        stack = new Mahasiswa22[size];
+        top = -1;
+    }
+
+    // isFull untuk mengecek apakah tumpukan tugas mahasiswa sudah terisi penuh sesuai kapasitas.
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // isEmpty untuk mengecek apakah tumpukan tugas masi kosong.
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // push untuk dapat menambahkan berkas tugas ke dalam tumpukan Stack
+    public void push(Mahasiswa22 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh ! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    // pop untuk mengeluarkan tugas yg akan dinilai. Mempunyai nilai kembalian berupa object dri class Mahasiswa22
+    public Mahasiswa22 pop() {
+        if (!isEmpty()) {
+            Mahasiswa22 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    // peek untuk dapat mengecek tumpukan tugas mahasiswa yg berada di posisi paling atas
+    public Mahasiswa22 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    // print untuk dapat menampilkan semua daftar tugas mahasiswa pada stack
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public Mahasiswa22 peekLower() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong!");
+            return null;
+        }
+    }
+
+    public int hitungTugas() {
+        return top + 1;
+    }
+}
