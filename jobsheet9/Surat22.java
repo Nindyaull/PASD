d6f71d89ac3726d5ba7a1af888c20e63c457e113
Nindyaull <pindangoreng123@gmail.com>
2026-04-24 23:00:49 +0700

    class surat (stack)

---
+++ jobsheet9/Surat22.java
@@ -0,0 +1,18 @@
+package jobsheet9;
+
+public class Surat22 {
+    String idSurat, namaMhs, kelas;
+    char jenisIzin;
+    int durasi;
+
+    public Surat22() {
+    }
+
+    public Surat22(String idSurat, String namaMhs, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMhs = namaMhs;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+}
